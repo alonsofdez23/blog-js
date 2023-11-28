1e155c2ec74b5f9bb501f85e8b3d0d0848c6300c
alonsofdez23 <alonsobs23@gmail.com>
2023-11-28 12:19:23 +0100

    fix formate date relative

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,26 @@
 export const formatDateRelative = (value) => {
-    const currentDate = new Date();
-    const postDate = new Date(value)
-    const time = Number.parseInt((postDate - currentDate) / 1000 / 60 / 60 / 24);
+    const postDate = new Date(value).getTime();
+    const currentDate = new Date().getTime();
+    const diff = currentDate - postDate;
 
-    return new Intl.RelativeTimeFormat('es-ES', {
-        numeric: 'auto'
-    }).format(time, 'day');
+    const seconds = diff / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+    const months = days / 30;
+    const years = months / 12;
+
+    if (seconds < 60) {
+        return `${Math.round(seconds)} segundos`;
+    } else if (minutes < 60) {
+        return `${Math.round(minutes)} minutos`;
+    } else if (hours < 24) {
+        return `${Math.round(hours)} horas`;
+    } else if (days < 30) {
+        return `${Math.round(days)} días`;
+    } else if (months < 12) {
+        return `${Math.round(months)} meses`;
+    } else {
+        return `${Math.round(years)} años`;
+    }
 }
