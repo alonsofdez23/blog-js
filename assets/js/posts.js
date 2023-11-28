1c64d6f0e53d1c49f6b20db0b50a1fba6926104c
alonsofdez23 <alonsobs23@gmail.com>
2023-11-28 11:46:22 +0100

    refactor generate posts

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,42 +1,62 @@
 import { urls } from "./constant.js";
 import { formatDateRelative } from "./utils.js";
 
-const data = await fetch(urls.post)
-    .then(res => res.json())
-    .then(data => data)
-    .catch(err => console.error(err));
-const { posts } = data;
+async function postsList() {
+    const { posts } = await fetch(urls.post)
+        .then(res => res.json())
+        .catch(err => console.error(err));
 
-const postsList = document.querySelector('.posts');
+    const postsList = document.querySelector('.posts');
 
-posts.forEach(post => {
-    console.log(post);
+    posts.forEach(post => {
+        postsList.appendChild(generatePost(post));
 
-    const article = document.createElement('article');
-    article.classList.add('post');
-    article.dataset.id = post.id;
-    const link = document.createElement('a');
-    link.classList.add('post-link');
-    const header = document.createElement('header');
-    header.classList.add('post-header');
-    const title = document.createElement('h3');
-    title.classList.add('post-title');
-    title.textContent = post.title;
-    const footer = document.createElement('footer');
-    footer.classList.add('post-footer');
-    const content = document.createElement('p');
-    content.classList.add('post-content');
-    content.textContent = post.content;
-    const date = document.createElement('p');
-    date.classList.add('post-date');
-    date.textContent = formatDateRelative(post.date);
+        const detailPost = document.querySelector('.detail-post');
 
-    header.appendChild(title);
-    footer.appendChild(content);
-    footer.appendChild(date);
-    link.appendChild(header);
-    link.appendChild(footer);
-    article.appendChild(link);
+        link.addEventListener('click', () => {
+            postsList.classList.add('hidden');
 
-    postsList.appendChild(article);
-});
+            detailPost.classList.remove('hidden');
+        });
+
+        const backButton = document.querySelector('.back-button');
+
+        backButton.addEventListener('click', () => {
+            postsList.classList.remove('hidden');
+
+            detailPost.classList.add('hidden');
+        });
+    });
+
+    function generatePost(post) {
+        const article = document.createElement('article');
+        article.classList.add('post');
+        article.dataset.id = post.id;
+        const link = document.createElement('a');
+        link.classList.add('post-link');
+        const header = document.createElement('header');
+        header.classList.add('post-header');
+        const title = document.createElement('h3');
+        title.classList.add('post-title');
+        title.textContent = post.title;
+        const footer = document.createElement('footer');
+        footer.classList.add('post-footer');
+        const content = document.createElement('p');
+        content.classList.add('post-content');
+        content.textContent = post.content;
+        const date = document.createElement('p');
+        date.classList.add('post-date');
+        date.textContent = formatDateRelative(post.date);
+
+        header.appendChild(title);
+        footer.appendChild(content);
+        footer.appendChild(date);
+        link.appendChild(header);
+        link.appendChild(footer);
+        article.appendChild(link);
+
+        return article;
+    }
+}
+
+postsList();
